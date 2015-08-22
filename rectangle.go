4d49801d97f1b9a7a2ce3ee29ff770b9febb8395
Steven Edwards <steven@stephenwithav.com>
2015-08-21 20:22:41 -0400

    Define Rectangle.

---
+++ rectangle.go
@@ -0,0 +1,50 @@
+package ssvgc
+
+import (
+	"encoding/xml"
+	"image"
+	"image/draw"
+)
+
+type Rectangle struct {
+	commonElement
+
+	rX float64
+	rY float64
+}
+
+func (r *Rectangle) ParseAttributes(start *xml.StartElement) {
+	r.parseCommonAttributes(start)
+
+	for _, attr := range start.Attr {
+		switch attr.Name.Local {
+		case "rx":
+			r.rX = parseFloatUnit(attr.Value)
+		case "ry":
+			r.rY = parseFloatUnit(attr.Value)
+		}
+	}
+}
+
+func (r *Rectangle) Draw() image.Image {
+	if r.upToDate && r.canvas != nil {
+		return r.canvas
+	}
+
+	fillBounds := r.Bounds()
+
+	switch r.strokeColor {
+	case image.Transparent:
+		r.canvas = image.NewRGBA(fillBounds)
+	default:
+		offsetBy := r.strokeWidth >> 1
+		strokeBounds := fillBounds.Inset(-offsetBy)
+		r.canvas = image.NewRGBA(strokeBounds)
+		draw.Draw(r.canvas, strokeBounds, &image.Uniform{r.strokeColor}, image.ZP, draw.Over)
+	}
+
+	draw.Draw(r.canvas, fillBounds, &image.Uniform{r.fillColor}, image.ZP, draw.Over)
+	r.upToDate = true
+
+	return r.canvas
+}
